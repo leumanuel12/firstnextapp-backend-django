3d690e2744aaf71af1c8b195b624c0d79d5bddca
leumanuel12 <cantiveros12@gmail.com>
2023-01-26 13:31:50 +0800

    added path for individual customer in urls

--- customers/urls.py
+++ customers/urls.py
@@ -20,4 +20,5 @@ from customers import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/customers/', views.customers, name='customers'),
+    path('api/customers/<int:id>', views.customer, name='customer'),
 ]
